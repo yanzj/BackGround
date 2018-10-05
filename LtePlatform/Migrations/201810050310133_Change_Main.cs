74538a8fd646e081a60dcbe606353c403c9c8f72
ouyh18 <ouyh18@189.cn>
2018-10-05 14:56:45 +0800

    2018-10-5 Refactor...

---
+++ LtePlatform/Migrations/201810050310133_Change_Main.cs
@@ -0,0 +1,16 @@
+namespace LtePlatform.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Change_Main : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
